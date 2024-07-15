c2211cbd46fa31160a27aaf112020a07d11b832c
MuhRandy <muhammadrandy0027@gmail.com>
2024-07-15 10:13:24 +0800

    initial init

---
+++ README.md
@@ -0,0 +1,25 @@
+# Portfolio Prakerja Muhammad Randy
+
+This repo is for completing final project on Hacktiv8.
+
+## Attribution
+
+### Icon
+
+I get the icon from [Tabler Icons](https://tabler.io/icons)
+
+### Illustration
+
+I get the svg illustration from [Undraw](https://undraw.co/)
+
+### Separator
+
+I get the wave separator from [Omatsuri](https://omatsuri.app/page-dividers)
+
+### Typed Animation
+
+I get the typed animation from [Typed.js](https://mattboldt.github.io/typed.js/)
+
+### Alert
+
+I get the alert from [Sweet Alert 2](https://sweetalert2.github.io/)
---
+++ tailwind-config.js
@@ -0,0 +1,44 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#6c63ff",
+          100: "#040046",
+          200: "#07008d",
+          300: "#0b00d3",
+          400: "#261bff",
+          500: "#6c63ff",
+          600: "#8781ff",
+          700: "#a5a0ff",
+          800: "#c3c0ff",
+          900: "#e1dfff",
+        },
+        secondary: {
+          DEFAULT: "#6efafb",
+          100: "#024747",
+          200: "#048d8d",
+          300: "#05d4d4",
+          400: "#28f9f9",
+          500: "#6efafb",
+          600: "#8cfcfc",
+          700: "#a8fdfd",
+          800: "#c5fefe",
+          900: "#e2fefe",
+        },
+        tersier: {
+          DEFAULT: "#fff4e4",
+          100: "#613a00",
+          200: "#c27400",
+          300: "#ffa724",
+          400: "#ffce85",
+          500: "#fff4e4",
+          600: "#fff7eb",
+          700: "#fff9f0",
+          800: "#fffbf5",
+          900: "#fffdfa",
+        },
+      },
+    },
+  },
+};
